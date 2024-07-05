c398d7168bdf38b4b969c39f422eabdf91116760
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-07-05 17:04:05 +0200

    feat: add static header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,10 +5,78 @@
 </script>
 
 <template>
-    <header>
-        <h1>Header</h1>
+    <header class="container">
+        <div id="box-image">
+            <a href="#">
+                <img src="../assets/dc-logo.png" alt="">
+            </a>
+        </div>
+
+        <!-- navigazione -->
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                <li>
+                    <a href="#">Item</a>
+                </li>
+                
+            </ul>
+        </nav>
+        
     </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+header{
+
+    padding: 10px;
+    @include flexbox();
+
+    #box-image img{
+        max-width: 70%;
+    }
+
+    ul{
+        display: flex;
+        gap: 20px;
+        list-style: none;
+
+        li a{
+            font-weight: 600;
+            text-decoration: none;
+        }
+    }
+
+
+}
+
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,10 +5,28 @@
 </script>
 
 <template>
-    
-    <h1>Corpo centrale</h1>
+
+    <section class="section-color">
+
+        <div id="content" class="container">
+            <h2>content</h2>
+        </div>
+
+    </section>
+
     
 </template>
 
 <style lang="scss" scoped>
+
+#content{
+
+    padding: 10px;
+    height: 100px;
+
+    h2{
+        color: white;
+        line-height: 70px
+    }
+}
 </style>

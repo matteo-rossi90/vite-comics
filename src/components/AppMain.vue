3f6062e74dfae970acbcee20a9ff61923f34c6c0
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-07-08 16:02:15 +0200

    feat: add static stylized cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,90 @@
 <script>
+
+import Cards from '../components/Cards.vue';
+
     export default{
-        name: 'AppMain'
+        name: 'AppMain',
+        components: {
+            Cards
+        },
+        data(){
+            return{
+                listComics: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+             ]
+                }
+            }
     }
 </script>
 
@@ -8,9 +92,20 @@
 
     <section class="section-color">
 
-        <!-- contenitore del jumbotron e delle cards -->
+        <!-- contenitore del jumbotron -->
+        <div id="jumbotron"></div>
+
+        <!-- contenitore delle cards -->
         <div id="content" class="container">
-            <h3>CURRENT SERIES</h3>
+
+            <div class="box-blue">
+                <h3>CURRENT SERIES</h3>
+            </div>
+
+            <div id="box-cards">
+                <Cards/>
+            </div>
+            
         </div>
 
     </section>
@@ -19,15 +114,36 @@
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+#jumbotron{
+
+    background-image: url('../assets/jumbotron.jpg');
+    background-size: cover;
+    height: 300px;
+}
 
 #content{
 
-    padding: 10px;
-    height: 100px;
+    padding: 10px 0;
 
     h3{
         color: white;
-        line-height: 70px
+        padding: 5px 0;
+        text-align: center;
+    }
+
+    .box-blue{
+        background-color: $merch;
+        width: 15%
+    }
+
+    #box-cards{
+        display: flex;
+        flex-wrap: wrap;
+        gap: 10px;
+        padding: 20px 0;
     }
 }
 </style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,119 @@
+<script>
+
+    export default{
+        name: 'Cards'
+    }
+
+</script>
+
+<template>
+
+    <div class="card-items">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+    <div class="card-items">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        <h3>Prezzo: 5 euro</h3>
+        <h4>Serie</h4>
+        <h5>Tipo</h5>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+.card-items{
+    width: calc((100% / 6) - 20px);
+    margin: 5px;
+    cursor: pointer;
+
+    img{
+        max-height: 200px;
+        width: 100%;
+        object-fit: cover;
+    }
+
+    h3,
+    h4,
+    h5{
+        color: white;
+    }
+}
+
+</style>
